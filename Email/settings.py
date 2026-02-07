dbcb4999145983d3ab05c42a8c6078e9ca85d051
SUNDHAR V <SVSsundhar143@gmail.com>
2026-02-08 00:11:46 +0530

    SMTP email backend configuration
    Configure email settings for SMTP using Gmail.

---
+++ Email/settings.py
@@ -0,0 +1,8 @@
+#add this at end of the code in settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER ='owlcodexdev@gmail.com'
+EMAIL_HOST_PASSWORD ='gdqa pseu nvtw yymp'
